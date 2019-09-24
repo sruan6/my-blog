b198bf1d53c89276577de3441e3f775b576e3383
Stephen Ruan <stephenruan@Stephens-MacBook-Pro.local>
2019-09-24 15:10:08 -0700

    test

--- server/index.js
+++ server/index.js
@@ -38,6 +38,14 @@ app.use(
 );
 server.applyMiddleware({ app });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(4000, () => {
   console.log(`🚀  Server ready at http://localhost:4000/`);
 });
