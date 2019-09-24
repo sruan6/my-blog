59403380786665c37a3c4c23cd33ff5f6f4f5c6b
Stephen Ruan <stephenruan@Stephens-MacBook-Pro.local>
2019-09-24 15:53:48 -0700

    new

--- config/devkeys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  FACEBOOK_APP_ID: '1328521540605202',
-  FACEBOOK_APP_SECRET: 'f6cdcadbfd4f3b455269e5bee1cb2056',
-  GOOGLE_CLIENT_ID:
-    '75123178868-j6m6e8oij9288bbftlkkrbdm2dpt5ltf.apps.googleusercontent.com',
-  GOOGLE_CLIENT_SECRET: 'IF-9fUkcm8tnl1WS8Lcene-c',
-  mongoURI: 'mongodb://username:password2@ds046267.mlab.com:46267/my-blog',
-  cookieKey: 'dfslhjsahfkhsalksfhadlkfdhlkdsj',
-};
